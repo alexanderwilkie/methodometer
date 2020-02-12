941794d3927af7de438d8e28fc2c9195901a9882
Alex <alexanderwilkie@gmail.com>
2020-02-11 20:00:46 -0800

    Initial Commit

---
+++ Methodometer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Methodometer
+//
+//  Created by Alex on 2020-02-11.
+//  Copyright © 2020 Holden Collective. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
